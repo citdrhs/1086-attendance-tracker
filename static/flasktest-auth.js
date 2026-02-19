14b446a55f4a09f38ad244aec24b808262f06365
Aditi <you@example.com>
2026-02-18 20:07:49 -0500

    making differences between login and sign up pages

--- static/flasktest-auth.js
+++ static/flasktest-auth.js
@@ -5,8 +5,27 @@ const submitBtn = document.getElementById("submitBtn");
 const form = document.getElementById("form");
 const msg = document.getElementById("msg");
 const title = document.getElementById("title");
+
+//checkboxes functionality 
 const checked = document.querySelectorAll('input[name="subteam"]:checked');
 const subteams = Array.from(checked).map(cb => cb.value);
+
+//sign up vs login tabs 
+const signupOnly  = document.querySelectorAll(".signup-only");
+
+tabLogin.onclick = () => {
+    tabLogin.classList.add("active");
+    tabSignup.classList.remove("active");
+
+    signupOnly.forEach(el => el.classList.add("hidden"));
+};
+
+tabSignup.onclick = () => {
+    tabSignup.classList.add("active");
+    tabLogin.classList.remove("active");
+
+    signupOnly.forEach(el => el.classList.remove("hidden"));
+};
 
 let mode = "login"; // or "signup"
 
@@ -63,3 +82,5 @@ msg.textContent = "Network error. Is the server running?";
 });
 
 setMode("login");
+
+signupOnly.forEach(el.classList.add("hidden"));
