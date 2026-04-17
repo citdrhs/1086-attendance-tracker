44db3dad0b52a1c539dd6be574e0dad264fbd33e
IbrahimA318 <hcps-alnaqshai@henricostudents.org>
2026-04-17 01:10:05 -0400

    Updated comments

--- db/static/flasktest-nav.js
+++ db/static/flasktest-nav.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => { //runs code when HTML is fully loaded
     const loggedIn = !!localStorage.getItem("member_id");
-    document.getElementById("nav-profile").style.display = loggedIn ? "inline-block" : "none";
-    document.getElementById("nav-signin").style.display = loggedIn ? "none" : "inline-block";
+    document.getElementById("nav-profile").style.display = loggedIn ? "inline-block" : "none";// shows profile button if logged in and hides it if not
+    document.getElementById("nav-signin").style.display = loggedIn ? "none" : "inline-block";//hides sign in button if logged in, shows it if not
 });
